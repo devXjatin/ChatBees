07ee2cbf4058038b18203aa6f28a18bfe8f5c55c
devXjatin <jatin13280@gmail.com>
2022-08-26 12:50:50 +0530

    registration screen, login authentication setup

--- app/src/main/java/com/example/chatbees/LoginActivity.java
+++ app/src/main/java/com/example/chatbees/LoginActivity.java
@@ -2,19 +2,67 @@ package com.example.chatbees;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     TextView txt_signup;
+    EditText loginEmail, loginPassword;
+    TextView signInBtn;
+    FirebaseAuth auth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         txt_signup = findViewById(R.id.txt_signup);
+        signInBtn = findViewById(R.id.signin_btn);
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+
+        //sign in button
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                //check credentials validation
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                }else if(!email.matches(emailPattern)){
+                    loginEmail.setError("Invalid Email");
+                }else if(password.length()>6){
+                    loginPassword.setError("Invalid Password");
+                    Toast.makeText(LoginActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                }else{
+                    //check authentication
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            }else{
+                                Toast.makeText(LoginActivity.this, "Error in Login", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
         txt_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/chatbees/RegistrationActivity.java
+++ app/src/main/java/com/example/chatbees/RegistrationActivity.java
@@ -1,15 +1,24 @@
 package com.example.chatbees;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationActivity extends AppCompatActivity {
-
-    @Override
+    TextView txt_signin;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        txt_signin = findViewById(R.id.txt_signin);
+        txt_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
